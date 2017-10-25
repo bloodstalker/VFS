dc88bb5c8e530a607080a9b86198a49c2a5ad937
bloodstalker <thabogre@gmail.com>
2017-10-25 05:50:00 +0330

    updated

--- ipfs.py
+++ ipfs.py
@@ -104,14 +104,45 @@ def syscall_304():
 
 
 def main():
-    client = ipfs_init()
-    res1 = add_file(client, 'test1.txt')
-    res2 = add_file(client, 'test2.txt')
-    res3 = add_file(client, 'test3.txt')
+    client = ipfsapi.connect('127.0.0.1', 5001)
+
+    client.files_mkdir("/test", parents=True)
+    client.files_mkdir("/test1", parents=True)
+
+    res1 = client.add('test1.c', wrap_with_directory=True)
+    res2 = client.add('test2.cpp', wrap_with_directory=True)
+    res3 = client.add('test3.wasm', wrap_with_directory=True)
+
+    file1_ipfs_addr = res1[1]['Hash'] + "/" + "test1.c"
+    file2_ipfs_addr = res2[1]['Hash'] + "/" + "test2.cpp"
+    file3_ipfs_addr = res3[1]['Hash'] + "/" + "test3.wasm"
+    print(client.cat(file1_ipfs_addr))
+    print(client.cat(file2_ipfs_addr))
+    #client.files_cp("/test/"+res1[0]['Hash'], "/test1")
+    #client.files_cp("/ipfs/"+res1[0]['Hash'], "/test1/test1.c")
+    #client.files_cp("/ipfs/"+res2[0]['Hash'], "/test1/test2.cpp")
+    #client.files_cp("/ipfs/"+res3[0]['Hash'], "/test1/test3.wasm")
 
     print(repr(res1))
-    print(repr(res2))
-    print(repr(res3))
+    print(repr(res1[0]['Hash']))
+
+    #print(client.pin_ls())
+    client.repo_gc()
+    #print(client.pin_ls())
+    print(client.repo_stat())
+    #print(client.swarm_peers())
+    print(client.files_ls("/test1"))
+
+    #print(client.cat("/test/" + res1[0]['Hash']))
+    #print(client.cat("/test/" + res1[0]['Hash']))
+    #print(client.cat("/test/" + res1[1]['Hash'] + "/test1.txt"))
+
+    #print(repr(res1))
+    #print(repr(res2))
+    #print(repr(res3))
+    #print(client.cat(res1['Hash']))
+    #print(client.cat(res2['Hash']))
+    #print(client.cat(res3['Hash']))
 
 
 if __name__ == '__main__':
